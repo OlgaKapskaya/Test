3a3ec0914f5c1084263f25e52a83fa8859d982a3
OlgaKapskaya <olikbuko@gmail.com>
2022-08-17 17:36:04 +0300

    CodeWars

---
+++ CodeWars/Build a pile of Cubes.js
@@ -0,0 +1,12 @@
+function findNb(m) {
+    let sum = 0;
+    let i=0;
+    while (sum < m){
+      i++;
+      sum += Math.pow(i,3);
+    }
+      if (sum == m){return i}
+      else {return -1};
+    }
+    
+    
