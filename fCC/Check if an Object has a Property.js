84604ef2b2d58388e8518f9401823cb989899b7c
OlgaKapskaya <olikbuko@gmail.com>
2022-08-21 18:01:37 +0300

    fCC

---
+++ fCC/Check if an Object has a Property.js
@@ -0,0 +1,28 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+if (("Alan" in userObj) && (userObj.hasOwnProperty("Jeff")) && ("Sarah" in userObj) && ("Ryan" in userObj)) {
+  return true;
+} else {
+  return false;
+}
+}
+
+console.log(isEveryoneHere(users));
